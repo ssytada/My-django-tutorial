cc41aa205a03956ba17bde39937460c9e7eb34e7
ssytada <105045839+ssytada@users.noreply.github.com>
2022-05-08 17:47:44 +0900

    complete part8(Authentication)

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,15 +1,21 @@
+import uuid  # Required for unique book instances
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+# Used to generate URLs by reversing the URL patterns
+from django.urls import reverse
+
+
 class Genre(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    name = models.CharField(
+        max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
-    
+
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -19,12 +25,15 @@ class Book(models.Model):
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    summary = models.TextField(
+        max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField(
+        'ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
-    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    genre = models.ManyToManyField(
+        Genre, help_text='Select a genre for this book')
 
     def __str__(self):
         """String for representing the Model object."""
@@ -41,14 +50,15 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'
 
 
-import uuid # Required for unique book instances
-
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -67,10 +77,19 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 
 class Author(models.Model):
     """Model representing an author."""
@@ -89,4 +108,3 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
-
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
