14811a7b7178a07e305cb2f6972d737c4aa64a8f
ssytada <105045839+ssytada@users.noreply.github.com>
2022-05-08 15:31:31 +0900

    complete part4(Admin Site)

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -34,6 +34,13 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
 import uuid # Required for unique book instances
 
 class BookInstance(models.Model):
@@ -82,3 +89,4 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
